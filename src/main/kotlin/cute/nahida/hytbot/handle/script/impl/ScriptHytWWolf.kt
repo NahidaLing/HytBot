15cecad8d240237abbedf64a672d60a013692d67
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-01-14 13:50:43 +0800

    refactor(Script): HytWWolf smart quit

--- src/main/kotlin/cute/nahida/hytbot/handle/script/impl/ScriptHytWWolf.kt
+++ src/main/kotlin/cute/nahida/hytbot/handle/script/impl/ScriptHytWWolf.kt
@@ -32,16 +32,15 @@ class ScriptHytWWolf: Script("WWolf", ScriptInfo(ScriptInfo.SuperAccountMode.MUL
     override fun onMessage(msg: String) {
         when {
             msg.contains("[狼人杀Ⅱ]") && msg.contains("加入了游戏") -> bot.script.status = Status.ROOM_WAIT_START
-            msg == "[狼人杀Ⅱ] 游戏开始！" -> {
-                bot.script.status = Status.ROOM_STARTED
-                if (!bot.script.superAccount) bot.sendMessage(HytBot.configManager.configs.message.on_game_started)
-            }
+            msg == "[狼人杀Ⅱ] 游戏开始！" -> if (!bot.script.superAccount) bot.sendMessage(HytBot.configManager.configs.message.on_game_started)
         }
     }
 
     override fun onTitle(title: String?, subTitle: String?) {
         when (title) {
             "花雨庭" -> bot.script.status = Status.HUB
+            // 只需要让狼人立刻退出
+            "§f本局你是..§c§l狼人!", "§e§l大吉大利!你获胜了!" -> bot.script.status = Status.ROOM_STARTED
         }
     }
 
