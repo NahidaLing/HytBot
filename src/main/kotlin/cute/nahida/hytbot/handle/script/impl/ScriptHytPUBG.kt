aa15dfb25aa4dd32077314f3c3272147bab28467
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-01-15 20:46:54 +0800

    fix(Script): HytPUBG join game check

--- src/main/kotlin/cute/nahida/hytbot/handle/script/impl/ScriptHytPUBG.kt
+++ src/main/kotlin/cute/nahida/hytbot/handle/script/impl/ScriptHytPUBG.kt
@@ -1,16 +1,14 @@
 package cute.nahida.hytbot.handle.script.impl
 
+import com.github.steveice10.mc.protocol.packet.ingame.server.scoreboard.ServerScoreboardObjectivePacket
+import com.github.steveice10.packetlib.packet.Packet
 import cute.nahida.hytbot.HytBot
-import cute.nahida.hytbot.handle.bots.bot.BotPosition
 import cute.nahida.hytbot.handle.script.Script
 import cute.nahida.hytbot.handle.script.ScriptInfo
 import cute.nahida.hytbot.handle.script.manager.ScriptHytJoinGameData
 import cute.nahida.hytbot.handle.script.utils.misc.Status
 
 class ScriptHytPUBG: Script("PUBG", ScriptInfo(ScriptInfo.SuperAccountMode.SINGLE)) {
-    companion object {
-        private val IN_GAME_POS = BotPosition(1377.0, 227.0, 22.0, 260.84973f, 21.749983f)
-    }
 
     init {
         joinGameManager.game = ScriptHytJoinGameData(4, "TEAM_FIGHT/pubg-solo")
@@ -24,10 +22,12 @@ class ScriptHytPUBG: Script("PUBG", ScriptInfo(ScriptInfo.SuperAccountMode.SINGL
         }
     }
 
-    override fun onTeleport(position: BotPosition) {
-        if (position == IN_GAME_POS && bot.script.status == Status.HUB) {
-            bot.script.status = Status.ROOM_WAIT_START
-            if (!bot.script.superAccount) bot.sendMessage(HytBot.configManager.configs.message.on_join_game)
+    override fun onPacket(packet: Packet) {
+        when(packet) {
+            is ServerScoreboardObjectivePacket -> if (packet.displayName == "§e§l代号:吃鸡" && bot.script.status == Status.HUB) {
+                bot.script.status = Status.ROOM_WAIT_START
+                if (!bot.script.superAccount) bot.sendMessage(HytBot.configManager.configs.message.on_join_game)
+            }
         }
     }
 }
